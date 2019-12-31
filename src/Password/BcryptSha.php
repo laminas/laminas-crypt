42d3ac8187ce547ba76dba22a36bdec7274e10a8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:33:03 -0600

    Rewriting as Laminas Project package

--- src/Password/BcryptSha.php
+++ src/Password/BcryptSha.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\Password;
+namespace Laminas\Crypt\Password;
 
-use Zend\Crypt\Hash;
+use Laminas\Crypt\Hash;
 
 /**
  * Bcrypt algorithm using crypt() function of PHP with password

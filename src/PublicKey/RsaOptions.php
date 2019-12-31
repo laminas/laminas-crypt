967a9e9540cf23257149eda331e5e28b6cf08419
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:31:53 -0600

    Rewriting as Laminas Project package

--- src/PublicKey/RsaOptions.php
+++ src/PublicKey/RsaOptions.php
@@ -1,25 +1,23 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Crypt
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\PublicKey;
+namespace Laminas\Crypt\PublicKey;
 
+use Laminas\Crypt\PublicKey\Rsa\Exception;
+use Laminas\Stdlib\AbstractOptions;
+use Laminas\Stdlib\ArrayUtils;
 use Traversable;
-use Zend\Crypt\PublicKey\Rsa\Exception;
-use Zend\Stdlib\AbstractOptions;
-use Zend\Stdlib\ArrayUtils;
 
 /**
  * RSA instance options
  *
- * @category   Zend
- * @package    Zend_Crypt
+ * @category   Laminas
+ * @package    Laminas_Crypt
  * @subpackage PublicKey
  */
 class RsaOptions extends AbstractOptions

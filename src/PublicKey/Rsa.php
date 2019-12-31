bd89ec605df28d159e563138a8d516a299a3d9aa
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:32:01 -0600

    Rewriting as Laminas Project package

--- src/PublicKey/Rsa.php
+++ src/PublicKey/Rsa.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\PublicKey;
+namespace Laminas\Crypt\PublicKey;
 
+use Laminas\Crypt\PublicKey\Rsa\Exception;
+use Laminas\Crypt\PublicKey\RsaOptions;
+use Laminas\Stdlib\ArrayUtils;
 use Traversable;
-use Zend\Crypt\PublicKey\RsaOptions;
-use Zend\Crypt\PublicKey\Rsa\Exception;
-use Zend\Stdlib\ArrayUtils;
 
 /**
  * Implementation of the RSA public key encryption algorithm.
@@ -40,7 +39,7 @@ class Rsa
     {
         if (!extension_loaded('openssl')) {
             throw new Exception\RuntimeException(
-                'Can not create Zend\Crypt\PublicKey\Rsa; openssl extension to be loaded'
+                'Can not create Laminas\Crypt\PublicKey\Rsa; openssl extension to be loaded'
             );
         }
 
@@ -102,7 +101,7 @@ class Rsa
     {
         if (!extension_loaded('openssl')) {
             throw new Exception\RuntimeException(
-                'Zend\Crypt\PublicKey\Rsa requires openssl extension to be loaded'
+                'Laminas\Crypt\PublicKey\Rsa requires openssl extension to be loaded'
             );
         }
 
--- test/Password/BcryptTest.php
+++ test/Password/BcryptTest.php
@@ -1,24 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Crypt
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Crypt\Password;
+namespace LaminasTest\Crypt\Password;
 
-use Zend\Crypt\Password\Bcrypt;
-use Zend\Config\Config;
-use Zend\Crypt\Password\Exception;
+use Laminas\Config\Config;
+use Laminas\Crypt\Password\Bcrypt;
+use Laminas\Crypt\Password\Exception;
 
 /**
- * @category   Zend
- * @package    Zend_Crypt
+ * @category   Laminas
+ * @package    Laminas_Crypt
  * @subpackage UnitTests
- * @group      Zend_Crypt
+ * @group      Laminas_Crypt
  */
 class BcryptTest extends \PHPUnit_Framework_TestCase
 {
@@ -71,7 +69,7 @@ class BcryptTest extends \PHPUnit_Framework_TestCase
 
     public function testWrongConstruct()
     {
-        $this->setExpectedException('Zend\Crypt\Password\Exception\InvalidArgumentException',
+        $this->setExpectedException('Laminas\Crypt\Password\Exception\InvalidArgumentException',
                                     'The options parameter must be an array or a Traversable');
         $bcrypt = new Bcrypt('test');
     }
@@ -84,7 +82,7 @@ class BcryptTest extends \PHPUnit_Framework_TestCase
 
     public function testSetWrongCost()
     {
-        $this->setExpectedException('Zend\Crypt\Password\Exception\InvalidArgumentException',
+        $this->setExpectedException('Laminas\Crypt\Password\Exception\InvalidArgumentException',
                                     'The cost parameter of bcrypt must be in range 04-31');
         $this->bcrypt->setCost('3');
     }
@@ -97,7 +95,7 @@ class BcryptTest extends \PHPUnit_Framework_TestCase
 
     public function testSetSmallSalt()
     {
-        $this->setExpectedException('Zend\Crypt\Password\Exception\InvalidArgumentException',
+        $this->setExpectedException('Laminas\Crypt\Password\Exception\InvalidArgumentException',
                                     'The length of the salt must be at lest ' . Bcrypt::MIN_SALT_SIZE . ' bytes');
         $this->bcrypt->setSalt('small salt');
     }
@@ -131,7 +129,7 @@ class BcryptTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals('$2y$14$MTIzNDU2Nzg5MDEyMzQ1NexAbOIUHkG6Ra.TK9QxHOVUhDxOe4dkW',
                                 $this->bcrypt->create($password));
         } else {
-            $this->setExpectedException('Zend\Crypt\Password\Exception\RuntimeException',
+            $this->setExpectedException('Laminas\Crypt\Password\Exception\RuntimeException',
                 'The bcrypt implementation used by PHP can contains a security flaw ' .
                 'using password with 8-bit character. ' .
                 'We suggest to upgrade to PHP 5.3.7+ or use passwords with only 7-bit characters'

9d3bbfbe20434ba86d10584915174cac5782c638
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:33:06 -0600

    Rewriting as Laminas Project package

--- src/PublicKey/Rsa/Exception/ExceptionInterface.php
+++ src/PublicKey/Rsa/Exception/ExceptionInterface.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\PublicKey\Rsa\Exception;
+namespace Laminas\Crypt\PublicKey\Rsa\Exception;
 
-use Zend\Crypt\Exception\ExceptionInterface as Exception;
+use Laminas\Crypt\Exception\ExceptionInterface as Exception;
 
 interface ExceptionInterface extends Exception
 {

7ef863f5b382364e60ef152d59757951c8e390b2
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:31:59 -0600

    Rewriting as Laminas Project package

--- src/Key/Derivation/Pbkdf2.php
+++ src/Key/Derivation/Pbkdf2.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Crypt
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\Key\Derivation;
+namespace Laminas\Crypt\Key\Derivation;
 
-use Zend\Crypt\Hmac;
+use Laminas\Crypt\Hmac;
 
 /**
  * PKCS #5 v2.0 standard RFC 2898
  *
- * @category   Zend
- * @package    Zend_Crypt
+ * @category   Laminas
+ * @package    Laminas_Crypt
  */
 class Pbkdf2
 {
--- src/PublicKey/Rsa/Exception/ExceptionInterface.php
+++ src/PublicKey/Rsa/Exception/ExceptionInterface.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Crypt
+ * @see       https://github.com/laminas/laminas-crypt for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-crypt/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-crypt/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Crypt\PublicKey\Rsa\Exception;
+namespace Laminas\Crypt\PublicKey\Rsa\Exception;
 
-use Zend\Crypt\Exception\ExceptionInterface as Exception;
+use Laminas\Crypt\Exception\ExceptionInterface as Exception;
 
 /**
- * @category   Zend
- * @package    Zend_Crypt
+ * @category   Laminas
+ * @package    Laminas_Crypt
  * @subpackage PublicKey
  */
 interface ExceptionInterface extends Exception
